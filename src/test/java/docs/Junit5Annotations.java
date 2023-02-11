a2586da60da54fb7a011dd9f1322d14fa49cbcde
dopew <denisorobinsky@gmail.com>
2023-02-11 18:34:24 +0300

    first commit

---
+++ src/test/java/docs/Junit5Annotations.java
@@ -0,0 +1,17 @@
+package docs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Junit5Annotations {
+    @Test
+    void someTest() {
+        System.out.println("SomeTest");
+        assertTrue(true);
+    }
+
+
+
+
+}
